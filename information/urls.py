08bb6b98e1894673a9e34df465c83e97d295bfbe
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2021-02-10 23:06:26 +0530

    blackhawks commit

--- information/urls.py
+++ information/urls.py
@@ -15,7 +15,11 @@ urlpatterns = [
     # outdoor
     path('plumbago', views.page_open_plumbago, name="page_open_plumbago"),
     path('ajuga', views.page_open_ajuga, name="page_open_ajuga"),
+    path('mango', views.page_open_mango, name="page_open_mango"),
+    path('hibiscus', views.page_open_hibiscus, name="page_open_hibiscus"),
     
     #tools
     path('trowel', views.page_open_trowel, name='page_open_trowel'),
+    path('secateurs', views.page_open_secateurs, name='page_open_secateurs'),
+    path('rake', views.page_open_rake, name='page_open_rake'),
 ]
--- information/views.py
+++ information/views.py
@@ -16,6 +16,8 @@ def outdoor(request):
 def tools(request):
     return render(request, 'info/tools.html')
 
+# indoor 
+
 def page_open_lemon(request):
     return render(request, 'indoor_plants/lemon.html')
 
@@ -24,6 +26,9 @@ def page_open_chilly(request):
 
 def page_open_curry(request):
     return render(request, 'indoor_plants/curry.html')
+    
+
+# outdoor
 
 def page_open_plumbago(request):
     return render(request, 'outdoor_plants/plumbago.html')
@@ -31,5 +36,19 @@ def page_open_plumbago(request):
 def page_open_ajuga(request):
     return render(request, 'outdoor_plants/ajuga.html')
 
+def page_open_hibiscus(request):
+    return render(request, 'outdoor_plants/hibiscus.html')
+
+def page_open_mango(request):
+    return render(request, 'outdoor_plants/mango.html')
+
+# tools
+
 def page_open_trowel(request):
     return render(request, 'gardening_tools/trowel.html')
+
+def page_open_secateurs(request):
+    return render(request, 'gardening_tools/secateurs.html')
+
+def page_open_rake(request):
+    return render(request, 'gardening_tools/rake.html')
