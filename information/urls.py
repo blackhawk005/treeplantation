812580677b3f0c1ebe77c834427a8279c6dfcbb0
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2021-02-10 12:26:44 +0530

    blackhawks commit

--- information/urls.py
+++ information/urls.py
@@ -7,8 +7,15 @@ urlpatterns = [
     path('indoor', views.indoor, name='indoor'),
     path('outdoor', views.outdoor, name='outdoor'),
     path('tools', views.tools, name='tools'),
+    # indoor
     path('lemon', views.page_open_lemon, name='page_open_lemon'),
+    path('chilly', views.page_open_chilly, name='page_open_chilly'),
     path('curry', views.page_open_curry, name='page_open_curry'),
+
+    # outdoor
     path('plumbago', views.page_open_plumbago, name="page_open_plumbago"),
+    path('ajuga', views.page_open_ajuga, name="page_open_ajuga"),
+    
+    #tools
     path('trowel', views.page_open_trowel, name='page_open_trowel'),
 ]
--- information/views.py
+++ information/views.py
@@ -19,11 +19,17 @@ def tools(request):
 def page_open_lemon(request):
     return render(request, 'indoor_plants/lemon.html')
 
+def page_open_chilly(request):
+    return render(request, 'indoor_plants/chilly.html')
+
 def page_open_curry(request):
     return render(request, 'indoor_plants/curry.html')
 
 def page_open_plumbago(request):
     return render(request, 'outdoor_plants/plumbago.html')
+
+def page_open_ajuga(request):
+    return render(request, 'outdoor_plants/ajuga.html')
 
 def page_open_trowel(request):
     return render(request, 'gardening_tools/trowel.html')
