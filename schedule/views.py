98af44931fe4328bc4644f70d24ba3c7b3eac712
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2021-03-06 00:37:34 +0530

    blackhawks commit

--- schedule/views.py
+++ schedule/views.py
@@ -54,6 +54,7 @@ def send_data(request):
         time = request.POST['hidden_time']
         place = request.POST['hidden_place']
         current_user = request.user.username
+        print(current_user)
         try:
             mydb = MySQLdb.connect(
                 "localhost",
@@ -69,7 +70,7 @@ def send_data(request):
         result = mycursor.fetchall()
         flag = 0
         for i in result:
-            if unique_id  and current_user in i:
+            if unique_id in i  and current_user in i:
                 print('already there')
                 flag = 1
                 break
