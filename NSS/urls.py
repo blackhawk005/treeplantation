f8e58d7b4b3886be926f2e67b6bdc56bea584b62
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2021-04-04 23:13:58 +0530

    blackhawks commit

--- NSS/urls.py
+++ NSS/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
-from django.urls.conf import url
+from django.conf.urls import url
 from django.conf import settings
 
 urlpatterns = [
