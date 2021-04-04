963b7fd62fb2f4167f2715fdc3daba3fb7901d39
Karthik Iyer <iyerkarthik19@siesgst.ac.in>
2021-04-04 23:18:39 +0530

    k_iyer

--- NSS/settings.py
+++ NSS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&a%k#g+bsqh$zvfy0jg7$e6s_1+^fizy0=z@t-m%z(s&=q8(@i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '192.168.29.132', 'treeasurenss.siesgst.ac.in']
 
