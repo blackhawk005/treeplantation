688c713d3f7d68983d9d9ff1656ab0fac56ea097
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2021-04-05 12:26:22 +0530

    blackhawks commit

--- schedule/views.py
+++ schedule/views.py
@@ -96,6 +96,15 @@ def send_data(request):
     t1.start()
     t2 = threading.Thread(target=user_check)
     t2.start()
+    today = date.today()
+    now = datetime.now()
+    print("now =", now)
+
+    rest_date_time = '2021-04-30 12:00:00'
+    then = datetime.strptime(rest_date_time, '%Y-%m-%d %H:%M:%S')
+    print("then=", then)
+    if now < then:
+        return render(request, "covid.html")
     if (request.method=='POST'):
         unique_id = request.POST['hidden_unique_id']
         event_name = request.POST['hidden_event_name']
