f0fda77d8eac3a31402a506576b1b8001ce22b3b
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2021-07-21 19:21:17 +0530

    blackhawks commit

--- NSS/settings.py
+++ NSS/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&a%k#g+bsqh$zvfy0jg7$e6s_1+^fizy0=z@t-m%z(s&=q8(@i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0:8000', '127.0.0.1', '192.168.29.132', 'treeasurenss.siesgst.ac.in', 'treeasurenss.herokuapp.com']
 
