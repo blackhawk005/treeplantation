44b73ed12e69505352a45f8c35535111604463d0
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2021-07-21 11:26:03 +0530

    blackhawks commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from schedule.views import report_event
 from django.db import models
 
 # Create your models here.
@@ -5,3 +6,8 @@ from django.db import models
 class users(models.Model):
     user = models.CharField(max_length=300)
     report = models.CharField(max_length=300)
+    reported_map = models.CharField(max_length=300)
+    report_event = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.user
--- schedule/models.py
+++ schedule/models.py
@@ -10,6 +10,8 @@ class tt(models.Model):
     event_name = models.CharField(max_length=300)
     image = models.CharField(max_length=100)
     reported = models.CharField(max_length=100)
+    def __str__(self):
+        return self.event_name
 
 class participants(models.Model):
     name = models.CharField(max_length=300)
@@ -19,6 +21,8 @@ class participants(models.Model):
     date = models.CharField(max_length=300)
     time = models.CharField(max_length=300)
     place = models.CharField(max_length=300)
+    def __str__(self):
+        return self.name
 
 class pastevents(models.Model):
     date = models.CharField(max_length=300)
@@ -29,4 +33,6 @@ class pastevents(models.Model):
     unique_id = models.CharField(max_length=300)
     event_name = models.CharField(max_length=300)
     image = models.CharField(max_length=100)
+    def __str__(self):
+        return self.event_name
 
