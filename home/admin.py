cf12be5699b550ce2ccd27595753cb9a13f4709f
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2021-07-21 11:36:15 +0530

    blackhawks commit

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from schedule.models import *
 from home.models import *
+from maps.models import *
 
 # Register your models here.
 
@@ -8,4 +9,5 @@ admin.site.register(participants)
 admin.site.register(pastevents)
 admin.site.register(users)
 admin.site.register(tt)
+admin.site.register(Blog)
 
