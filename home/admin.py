f4cee8afefe646e835d4867edc2d357406bd30d8
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2021-07-21 11:51:46 +0530

    blackhawks commit

--- home/admin.py
+++ home/admin.py
@@ -4,15 +4,26 @@ from home.models import *
 from maps.models import *
 
 # Register your models here.
+class ParticipantsAdmin(admin.ModelAdmin):
+    # a list of displayed columns name.
+    list_display = ['name', 'event_name']
+admin.site.register(participants, ParticipantsAdmin)
 
-admin.site.register(participants)
-admin.site.register(pastevents)
+class PastEventsAdmin(admin.ModelAdmin):
+    # a list of displayed columns name.
+    list_display = ['host', 'event_name']
+admin.site.register(pastevents, PastEventsAdmin)
+
 admin.site.register(users)
-admin.site.register(tt)
+
+class TtAdmin(admin.ModelAdmin):
+    # a list of displayed columns name.
+    list_display = ['host', 'event_name']
+admin.site.register(tt, TtAdmin)
 
 class BlogAdmin(admin.ModelAdmin):
     # a list of displayed columns name.
     list_display = ['address', 'user']
-    
+
 admin.site.register(Blog, BlogAdmin)
 
