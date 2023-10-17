d17f17236ecc3ea5afddd70613e5155ab82e8425
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2023-10-17 18:49:39 -0400

    Debug = False

--- NSS/settings.py
+++ NSS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&a%k#g+bsqh$zvfy0jg7$e6s_1+^fizy0=z@t-m%z(s&=q8(@i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 host = ""
 
 ALLOWED_HOSTS = ['*']
