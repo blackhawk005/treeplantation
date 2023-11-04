a7e9cf83023e37788b7fc4ee62ec23557650df45
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2023-11-04 11:59:57 -0400

    final commit

--- NSS/urls.py
+++ NSS/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib import auth
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.static import serve
-from django.conf.urls import url
 from django.conf import settings
 from django.contrib.auth import views as auth_views
 
@@ -30,6 +29,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 
 
-    url(r'^media/(?P<path>.*)$',  serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root':       settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$',  serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root':       settings.STATIC_ROOT}), 
 ]
